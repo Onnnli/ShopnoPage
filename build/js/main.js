9e792d5091fc5815e3b373203fd59df169f52d34
Elizaveta O <souslayk@gmail.com>
2020-11-01 22:57:20 +0300

    service

--- build/js/main.js
+++ build/js/main.js
@@ -1,20 +1,60 @@
-$('.video').parent().click(function () {
-
-  if ($(this).children(".video").get(0).paused) {
-    $(this).children(".video").get(0).play();
-    $(this).children(".playpause").fadeOut();
-
-  } else {
-    $(this).children(".video").get(0).pause();
-
-    $(this).children(".playpause").fadeIn();
-
-  }
-
-});
-
-
 $('.burger').click(function(event) {
   $('.burger, .nav').toggleClass('active');
   $('body').toggleClass('.lock');
 });
+
+
+$('.more__one').click(function(event) {
+  $('.more__one, .active__one').toggleClass('active');
+  
+});
+
+$('.close__one').click(function(event) {
+  $('.close__one, .active__one').removeClass('active');
+});
+
+$('.more__two').click(function(event) {
+  $('.more__two, .active__two').toggleClass('active');
+  
+});
+
+$('.close__two').click(function(event) {
+  $('.close__two, .active__two').removeClass('active');
+});
+
+$('.more__three').click(function(event) {
+  $('.more__three, .active__three').toggleClass('active');
+  
+});
+
+$('.close__three').click(function(event) {
+  $('.close__three, .active__three').removeClass('active');
+});
+
+$('.more__four').click(function(event) {
+  $('.more__four, .active__four').toggleClass('active');
+  
+});
+
+$('.close__four').click(function(event) {
+  $('.close__four, .active__four').removeClass('active');
+});
+
+$('.more__five').click(function(event) {
+  $('.more__five, .active__five').toggleClass('active');
+  
+});
+
+$('.close__five').click(function(event) {
+  $('.close__five, .active__five').removeClass('active');
+});
+
+$('.more__six').click(function(event) {
+  $('.more__six, .active__six').toggleClass('active');
+  
+});
+
+$('.close__six').click(function(event) {
+  $('.close__six, .active__six').removeClass('active');
+});
+
